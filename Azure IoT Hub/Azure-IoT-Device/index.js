f2c28f3dc9ade6ef20de48253f35ae4914188171
vedant93 <vedantkumar.007@gmail.com>
2019-05-16 13:47:04 -0500

    Azure IoT Hub Intrusion Alert

---
+++ Azure IoT Hub/Azure-IoT-Device/index.js
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+
+// The device connection string to authenticate the device with your IoT hub.
+//
+// NOTE:
+// For simplicity, this sample sets the connection string in code.
+// In a production environment, the recommended approach is to use
+// an environment variable to make it available to your application
+// or use an HSM or an x509 certificate.
+// https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
+//
+// Using the Azure CLI:
+// az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+var connectionString = 'paste your full connection string here';
+
+// Using the Node.js Device SDK for IoT Hub:
+//   https://github.com/Azure/azure-iot-sdk-node
+// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message;
+
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+// Create a message and send it to the IoT hub every second
+
+var gpio = require('onoff').Gpio;
+var pir = new gpio(4, 'in', 'both');
+
+pir.watch(function(err, value) {
+    if (value == 1) {
+        var tempTime = new Date();
+		var currentTime = tempTime.toTimeString().substring(0, 8);
+
+	var message = new Message(JSON.stringify({
+    "Intrusion Alert": currentTime
+  }));
+
+  // Add a custom application property to the message.
+  // An IoT hub can filter on these properties without access to the message body.
+
+
+  console.log('Sending message: ' + message.getData());
+
+  // Send the message.
+  client.sendEvent(message, function (err) {
+    if (err) {
+      console.error('send error: ' + err.toString());
+    } else {
+      console.log('message sent');
+    }
+  });
+    }
+});
